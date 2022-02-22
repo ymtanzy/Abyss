36f60cfc16b9df87fc9de7c82d3d9c2b0b515391
thmslhst <tlhoest@gmail.com>
2022-02-22 17:02:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ var oscServer = new osc.Server(5555, '192.168.12.101', () => {
 });
 
 oscServer.on('message', function (msg) {
-  console.log(`Message: ${msg}`);
+  //console.log(`Message: ${msg}`);
   //oscServer.close();
 
   output.send('cc', {
@@ -28,20 +28,32 @@ oscServer.on('message', function (msg) {
   })
 });
 
-// setInterval(()=>{
+/* setInterval(()=>{
 
-//   let t = Date.now();
-//   let frequency = 0.0001;
-//   let amplitude = 127;
-//   let sine = 0;
+  let t = Date.now();
+  let frequency = 0.0001;
+  let amplitude = 127;
+  let sine = 0;
 
-//   for (let i = 0; i < 16; i++) {
-//     sine = Math.floor(((Math.sin(t*frequency*i)*amplitude)+amplitude)/2);
-//     output.send('cc', {
-//       controller: i,
-//       value: sine,
-//       channel: 0
-//     })
-//   }
+  for (let i = 0; i < 16; i++) {
+    sine = Math.floor(((Math.sin(t*frequency*i)*amplitude)+amplitude)/2);
+    output.send('cc', {
+      controller: i,
+      value: sine,
+      channel: 0
+    })
+  }
 
-// }, 10);
+}, 10);
+ */
+/* setInterval(()=>{
+
+  for (let i = 0; i < 16; i++) {
+    output.send('cc', {
+      controller: i,
+      value: Math.random()*127,
+      channel: 0
+    })
+  }
+
+}, 2000); */
